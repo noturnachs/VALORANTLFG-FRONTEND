cb576a2b6901973e20a652063cbfea0e451a21f9
Lius <142863069+noturnachs@users.noreply.github.com>
2024-07-13 20:25:01 +0800

    Update PartyFinder.jsx

--- src/components/PartyFinder.jsx
+++ src/components/PartyFinder.jsx
@@ -529,7 +529,7 @@ const PartyFinder = () => {
                   >
                     {party.expired ? "Expired" : "Active"}{" "}
                     <span className="text-white">-</span>&nbsp;
-                    <span className="bg-yellow-100 text-yellow-800 text-sm font-regular me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">
+                    <span className="bg-yellow-100 text-yellow-800 text-sm font-regular me-2 px-1 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">
                       {party.server_tag} Server
                     </span>{" "}
                   </p>
